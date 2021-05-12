3b84915d84d862ef79056521804b09d391645fe4
Ajay Prajapati <ajayprazz@gmail.com>
2021-05-12 19:22:04 +0545

    add fix for ob_end_flush error

--- wp-content/themes/photosofnepal/src/filter.php
+++ wp-content/themes/photosofnepal/src/filter.php
@@ -160,3 +160,15 @@ add_action( 'acf/save_post', 'photography_delete_request_approval' );
 //}
 //
 //add_action( 'pre_get_posts', 'include_tags_in_search' );
+
+/**
+ * Proper ob_end_flush() for all levels
+ *
+ * This replaces the WordPress `wp_ob_end_flush_all()` function
+ * with a replacement that doesn't cause PHP notices.
+ */
+remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
+add_action( 'shutdown', function () {
+	while ( @ob_end_flush() ) {
+	}
+} );
