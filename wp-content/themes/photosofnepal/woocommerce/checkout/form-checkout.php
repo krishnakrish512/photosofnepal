bf8cb14c6739eac9c3ed6f94e62889a6aab8e7a6
Ajay Prajapati <ajayprazz@gmail.com>
2021-05-27 18:20:34 +0545

    update checkout page

--- wp-content/themes/photosofnepal/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/photosofnepal/woocommerce/checkout/form-checkout.php
@@ -19,45 +19,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
-	$error = false;
-	if ( empty( $_POST['billing_first_name'] ) || empty( $_POST['billing_email'] ) || empty( $_POST['billing_phone'] ) ) {
-		$error = true;
-	}
-
-	if ( ! $error ) {
-		$cart_products = WC()->cart->get_cart();
-
-		$product_details = '';
-
-		foreach ( $cart_products as $product ) {
-			$product_details .= "Id: " . $product['product_id'] . "  Size: " . $product['variation']['attribute_pa_resolution'] . "<br/>";
-		}
-
-		$to = 'chandra@nirvanstudio.com';
-//	$to      = 'ajayprazz@gmail.com';
-		$subject = 'ImagePasal Inquiry';
-
-		$body = "Inquirer Details:<br/>";
-		$body .= "Name: " . $_POST['billing_first_name'] . "<br/>";
-		$body .= "Email: " . $_POST['billing_email'] . "<br/>";
-		$body .= "Phone Number: " . $_POST['billing_phone'] . "<br/>";
-		$body .= "Address: " . $_POST['billing_address_1'] . "<br/>";
-		$body .= "Country: " . $_POST['billing_country'] . "<br/>";
-		$body .= "Company: " . $_POST['billing_company'] . "<br/>";
-		$body .= "Company Email: " . $_POST['billing_company_email'] . "<br/><br/>";
-		$body .= "Inquiry For:<br/>";
-		$body .= $product_details;
-
-		$headers = array( 'Content-Type: text/html; charset=UTF-8' );
-
-		$success = wp_mail( $to, $subject, $body, $headers );
-	}
-
-//	var_dump( $success );
-
-}
-
 do_action( 'woocommerce_before_checkout_form', $checkout );
 
 // If checkout registration is disabled and not logged in, the user cannot checkout.
@@ -69,8 +30,8 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 ?>
 
-<form name="checkout" method="post" class="woocommerce-checkout"
-      action="" enctype="multipart/form-data">
+<form name="checkout" method="post" class="checkout woocommerce-checkout"
+      action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
     <div class="row">
         <div class="col-lg-7 col-md-6 ">
             <div class="purchase-form">
@@ -95,23 +56,10 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 				<?php endif; ?>
 
-                <div class="payment-option mb-4 d-none">
+                <div class="payment-option mb-4">
                     <h4 class="mb-4">Payment Option</h4>
 					<?php woocommerce_checkout_payment(); ?>
                 </div>
-                <div class="payment-option mb-4">
-                    <button class="btn btn-primary" type="submit">Inquiry</button>
-                </div>
-
-				<?php
-				if ( isset( $success ) ) {
-					if ( $success ) {
-						echo "<p>Inquiry sent successfully</p>";
-					} else {
-						echo "<p>Failed Sending inquiry</p>";
-					}
-				}
-				?>
             </div>
         </div>
 		<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
