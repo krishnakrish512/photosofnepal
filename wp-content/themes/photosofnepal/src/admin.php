4af02f7455898087119fd528667fc7f611958292
Ajay Prajapati <ajayprazz@gmail.com>
2021-06-02 18:22:37 +0545

    add featured product filter in admin

--- wp-content/themes/photosofnepal/src/admin.php
+++ wp-content/themes/photosofnepal/src/admin.php
@@ -68,3 +68,36 @@ if ( wc_current_user_has_role( 'wc_product_vendors_admin_vendor' ) || wc_current
 		return $default;
 	} );
 }
+
+//add featured product filter in admin
+add_action( 'restrict_manage_posts', 'featured_products_sorting' );
+function featured_products_sorting() {
+	global $typenow;
+	$post_type = 'product'; // change to your post type
+	$taxonomy  = 'product_visibility'; // change to your taxonomy
+	if ( $typenow == $post_type ) {
+		$selected      = isset( $_GET[ $taxonomy ] ) ? $_GET[ $taxonomy ] : '';
+		$info_taxonomy = get_taxonomy( $taxonomy );
+		wp_dropdown_categories( array(
+			'show_option_all' => __( "Show all {$info_taxonomy->label}" ),
+			'taxonomy'        => $taxonomy,
+			'name'            => $taxonomy,
+			'orderby'         => 'name',
+			'selected'        => $selected,
+			'show_count'      => true,
+			'hide_empty'      => true,
+		) );
+	};
+}
+
+add_filter( 'parse_query', 'featured_products_sorting_query' );
+function featured_products_sorting_query( $query ) {
+	global $pagenow;
+	$post_type = 'product'; // change to your post type
+	$taxonomy  = 'product_visibility'; // change to your taxonomy
+	$q_vars    = &$query->query_vars;
+	if ( $pagenow == 'edit.php' && isset( $q_vars['post_type'] ) && $q_vars['post_type'] == $post_type && isset( $q_vars[ $taxonomy ] ) && is_numeric( $q_vars[ $taxonomy ] ) && $q_vars[ $taxonomy ] != 0 ) {
+		$term                = get_term_by( 'id', $q_vars[ $taxonomy ], $taxonomy );
+		$q_vars[ $taxonomy ] = $term->slug;
+	}
+}
