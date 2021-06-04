71b1b676c1e55ce332fc1a027d1a8dd4b641ef4d
Ajay Prajapati <ajayprazz@gmail.com>
2021-06-04 19:32:04 +0545

    changes

--- wp-content/themes/photosofnepal/partials/flexible-content/photographs.php
+++ wp-content/themes/photosofnepal/partials/flexible-content/photographs.php
@@ -61,36 +61,3 @@ $photo_ids = get_sub_field( 'photographs' );
         </div>
     </div>
 </section>
-<div class="modal fade" id="exampleModal" class="popup-form" tabindex="-1" aria-labelledby="exampleModalLabel"
-     aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="exampleModalLabel">Sign Up</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="" class="sign-up-form">
-                    <div class="form-group">
-                        <label> Email</label>
-                        <input type="text" class="form-control" placeholder="Email Address">
-                    </div>
-                    <div class="form-group">
-                        <label> Password</label>
-                        <input type="password" class="form-control" placeholder="password">
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" value="Continue" class="btn btn-primary d-block w-100">
-                    </div>
-                </form>
-                <p class="mb-4">By creating an account, I agree to Photosofnepal's Website terms, Privacy policy and
-                    Licensing terms.</p>
-            </div>
-            <div class="modal-footer">
-                <p>Already have an account? <a href="#"> Log in</a></p>
-            </div>
-        </div>
-    </div>
-</div>
