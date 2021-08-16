f50ae20ba662862eb7a9e8bdf3f5a22dbe2eb455
Ajay Prajapati <ajayprazz@gmail.com>
2021-08-16 19:12:54 +0545

    update photograph buy button

--- wp-content/themes/photosofnepal/woocommerce/content-product.php
+++ wp-content/themes/photosofnepal/woocommerce/content-product.php
@@ -15,95 +15,95 @@
  * @version 3.6.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 global $product;
 
 // Ensure visibility.
-if ( empty( $product ) || ! $product->is_visible() ) {
-	return;
+if (empty($product) || !$product->is_visible()) {
+    return;
 }
 ?>
-<div <?php wc_product_class( 'discover-photos__grid-item', $product ); ?>>
-	<?php
-	$image_id = $product->get_image_id();
+<div <?php wc_product_class('discover-photos__grid-item', $product); ?>>
+    <?php
+    $image_id = $product->get_image_id();
 
-	$args = [
-		'post_type'   => 'gallery',
-		'post_status' => 'publish',
-		'meta_query'  => [
-			[
-				'key'     => 'photographs',
-				'value'   => $image_id,
-				'compare' => 'LIKE'
-			]
-		],
-	];
+    $args = [
+        'post_type' => 'gallery',
+        'post_status' => 'publish',
+        'meta_query' => [
+            [
+                'key' => 'photographs',
+                'value' => $image_id,
+                'compare' => 'LIKE'
+            ]
+        ],
+    ];
 
-	$gallery = get_posts( $args );
-	if ( ! empty( $gallery ) ) {
-		$gallery_url = get_permalink( $gallery[0] );
-	}
-	?>
-    <img src="<?= wp_get_attachment_image_url( $image_id, 'photography_thumbnail' ) ?>"
+    $gallery = get_posts($args);
+    if (!empty($gallery)) {
+        $gallery_url = get_permalink($gallery[0]);
+    }
+    ?>
+    <img src="<?= wp_get_attachment_image_url($image_id, 'photography_thumbnail') ?>"
          alt="<?= $product->get_title() ?>"/>
     <figcaption>
         <div class="figure-tools">
             <div class="figure-icons">
-				<?php echo do_shortcode( "[ti_wishlists_addtowishlist product_id='" . $product->get_id() . "']" ); ?>
-				<?php
-				if ( ! empty( $gallery ) ):
-					?>
-                    <a href="<?= esc_url( $gallery_url ) ?>"> <span class="icon-stack"></span></a>
-				<?php
-				endif;
-				?>
+                <?php echo do_shortcode("[ti_wishlists_addtowishlist product_id='" . $product->get_id() . "']"); ?>
+                <?php
+                if (!empty($gallery)):
+                    ?>
+                    <a href="<?= esc_url($gallery_url) ?>"> <span class="icon-stack"></span></a>
+                <?php
+                endif;
+                ?>
             </div>
         </div>
 
         <div class="figure-info ">
             <h6 class="font-weight-light mb-0"><?= $product->get_title() ?></h6>
-            <a href="<?= get_photogtaphy_buy_url( $product ) ?>" class="btn btn-primary btn-sm">Buy</a>
+            <a href="<?= esc_url($product->get_permalink()) ?>" class="btn btn-primary btn-sm">Buy</a>
         </div>
     </figcaption>
     <a href="<?= $product->get_permalink() ?>" class="stretched-link"></a>
-	<?php
-	/**
-	 * Hook: woocommerce_before_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_open - 10
-	 */
-	//	do_action( 'woocommerce_before_shop_loop_item' );
+    <?php
+    /**
+     * Hook: woocommerce_before_shop_loop_item.
+     *
+     * @hooked woocommerce_template_loop_product_link_open - 10
+     */
+    //	do_action( 'woocommerce_before_shop_loop_item' );
 
-	/**
-	 * Hook: woocommerce_before_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_show_product_loop_sale_flash - 10
-	 * @hooked woocommerce_template_loop_product_thumbnail - 10
-	 */
-	//	do_action( 'woocommerce_before_shop_loop_item_title' );
+    /**
+     * Hook: woocommerce_before_shop_loop_item_title.
+     *
+     * @hooked woocommerce_show_product_loop_sale_flash - 10
+     * @hooked woocommerce_template_loop_product_thumbnail - 10
+     */
+    //	do_action( 'woocommerce_before_shop_loop_item_title' );
 
-	/**
-	 * Hook: woocommerce_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_product_title - 10
-	 */
-	//	do_action( 'woocommerce_shop_loop_item_title' );
+    /**
+     * Hook: woocommerce_shop_loop_item_title.
+     *
+     * @hooked woocommerce_template_loop_product_title - 10
+     */
+    //	do_action( 'woocommerce_shop_loop_item_title' );
 
-	/**
-	 * Hook: woocommerce_after_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_rating - 5
-	 * @hooked woocommerce_template_loop_price - 10
-	 */
-	//	do_action( 'woocommerce_after_shop_loop_item_title' );
+    /**
+     * Hook: woocommerce_after_shop_loop_item_title.
+     *
+     * @hooked woocommerce_template_loop_rating - 5
+     * @hooked woocommerce_template_loop_price - 10
+     */
+    //	do_action( 'woocommerce_after_shop_loop_item_title' );
 
-	/**
-	 * Hook: woocommerce_after_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_close - 5
-	 * @hooked woocommerce_template_loop_add_to_cart - 10
-	 */
-	//	do_action( 'woocommerce_after_shop_loop_item' );
-	?>
+    /**
+     * Hook: woocommerce_after_shop_loop_item.
+     *
+     * @hooked woocommerce_template_loop_product_link_close - 5
+     * @hooked woocommerce_template_loop_add_to_cart - 10
+     */
+    //	do_action( 'woocommerce_after_shop_loop_item' );
+    ?>
 </div>
